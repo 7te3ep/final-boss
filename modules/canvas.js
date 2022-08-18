371d76f0a0a0c2df8547eafb9558e9a685645286
LiamOsler <liam.osler@dal.ca>
2022-08-18 17:33:30 -0300

    add canvas to module

--- modules/canvas.js
+++ modules/canvas.js
@@ -1,1 +1,5 @@
 //Canvas.js
+const c = document.getElementById("canvas");
+const ctx = c.getContext("2d");
+
+export {c, ctx};
--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 //scripts.js
 //Main script for the page
 
-import { } from "./modules/canvas.js";
+import {c, ctx} from "./modules/canvas.js";
 import { } from "./modules/environment.js";
 import { } from "./modules/game.js";
 import { } from "./modules/player.js";
